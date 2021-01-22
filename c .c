c42c9e184186e9718f3313117621524f933e1722
Vinod Seervi <65346730+vinodseervi@users.noreply.github.com>
2021-01-22 21:25:09 +0530

    dada type

---
+++ c .c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+
+
+int main()
+{
+	int age;
+	printf("Enter your festival name\n");
+	
+	scanf("%d",&age);
+	printf("Hey %d Everyone\n Happy Dipawali to you and your family\n Enjoy beautiful moments of this festival",age);
+	return 0;
+}
---
+++ datatype.c
@@ -0,0 +1,11 @@
+ #include<stdio.h>
+ 
+ main(){
+ 	
+ 	int integer = 999999999;
+ 	printf("int %d",integer);
+ 	
+ 	
+ 	long biginteger = 999999999;
+ 	printf("\n long %ld", biginteger);
+ }
